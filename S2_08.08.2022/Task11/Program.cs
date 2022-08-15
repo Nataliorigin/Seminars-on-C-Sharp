266c3c4d21fe05ef298cf19e69b09b33e2ac3589
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-08-15 16:51:23 +0500

    11

--- S2_08.08.2022/Task11/Program.cs
+++ S2_08.08.2022/Task11/Program.cs
@@ -6,8 +6,8 @@
 // 918 -> 98
 
 int num = new Random().Next(99, 1000);
-Console.WriteLine("Трехзначное число: "+ num);
+Console.WriteLine("Трехзначное число: " + num);
 int first_num = num / 100;
 int second_num = num % 10;
 int result = first_num * 10 + second_num;
-Console.WriteLine($"Ответ: "+result);
+Console.WriteLine($"Ответ: " + result);
