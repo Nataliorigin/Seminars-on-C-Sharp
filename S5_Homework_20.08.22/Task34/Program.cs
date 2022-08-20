32074cab816c6731069ba4006bee389831b6e22d
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-08-20 23:15:30 +0500

    Homework4 done 20/08/22

---
+++ S5_Homework_20.08.22/Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void MasRandom(int[] array) //Метод для: Создаем массив с рандомными трехзначными положительными числами
+{	
+	Random rmd = new Random();
+		for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rmd.Next(100, 1000);
+	}
+}
+
+void PrintArray(int[] array)  //Метод для: Выводим массив в консоль
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i < array.Length - 1) Console.Write($"{array[i]},");
+		else Console.Write(array[i]);
+	}
+	Console.Write("]");
+	Console.WriteLine();
+}
+
+int EvenElem(int[] randomArray) //Метод для: Подсчет количества четных чисел в массиве
+{
+	int count = 0;
+	for (int i = 0; i < randomArray.Length; i++)
+	{
+		if (randomArray[i] % 2 == 0)
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+int [] newarray = new int[7];
+MasRandom(newarray);
+Console.Write($"Количество четных элементов в массиве ");
+PrintArray(newarray);
+Console.Write($"Ответ: {EvenElem(newarray)}.");
---
+++ S5_Homework_20.08.22/Task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6]-> 0
+
+void MasRandom(int[] array) //Метод для: Создаем массив с рандомными двухзначными положительными числами
+{
+	Random rmd = new Random();
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rmd.Next(-10, 100);
+	}
+}
+void PrintArray(int[] array)  //Метод для: Выводим массив в консоль
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i < array.Length - 1) Console.Write($"{array[i]},");
+		else Console.Write(array[i]);
+	}
+	Console.Write("]");
+	Console.WriteLine();
+}
+int SumГnevenElem(int[] array) //Метод для: Сумма чисел на нечетных позициях
+{
+	int sumEl = 0;
+
+	for (int i = 1; i < array.Length; i += 2)
+	{
+		sumEl = array[i] + sumEl;
+	}
+	return sumEl;
+}
+int[] newarray = new int[5];
+MasRandom(newarray);
+Console.Write($"Массив: ");
+PrintArray(newarray);
+Console.Write($"Сумма чисел на нечетных позициях: {SumГnevenElem(newarray)}.");
