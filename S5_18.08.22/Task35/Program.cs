a09260596237e0c93ad4658eb57848f6deaea84f
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-08-20 19:09:23 +0500

    Seminar5

---
+++ S5_18.08.22/Task35/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2]-> 0
+// [10, 11, 12, 13, 14]-> 5
+
+int[] CreateArrayRndInt(int size, int min, int max) //Метод для создания массива случайных чисел
+{
+	int[] array = new int[size];
+	Random rnd = new Random();
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rnd.Next(min, max + 1);
+	}
+	return array;
+}
+void PrintArray(int[] array)	//Метод для выведения массива
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+		else Console.Write(array[i]);
+	}
+	Console.Write("]");
+	Console.WriteLine();
+} 
+
+int FindElem(int[] randomArray) //Метод для нахождения количества чисел в массиве на отрезке [10,99]
+{
+	int count = 0;
+	for (int i = 0; i < randomArray.Length; i++)
+	{
+		if (randomArray[i] >=10 && randomArray[i] <= 99)
+		{
+			count++;
+		}
+			}
+	return count;
+}
+
+int[] array = CreateArrayRndInt(5, 1, 100);
+PrintArray(array);
+Console.WriteLine(FindElem(array));
---
+++ S5_18.08.22/Task37/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6]-> 36 21
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+	int[] array = new int[size];
+	Random rnd = new Random();
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rnd.Next(min, max + 1);
+	}
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i < array.Length - 1) Console.Write($"{array[i]},");
+		else Console.Write(array[i]);
+	}
+	Console.Write("]");
+	Console.WriteLine();
+}
+
+int [] ProdPairsNumbers(int[] findarray)
+{
+	int prod = 0;
+	if(findarray.Length%2 ==0) prod = findarray.Length / 2;
+	else prod = (findarray.Length / 2) + 1;
+	int[] result = new int[prod];
+	for (int i = 0; i < findarray.Length/2; i++)
+	{
+			result[i] = findarray[i] * findarray[findarray.Length -i-1];	
+	}
+	if(findarray.Length%2 !=0)
+		result[prod - 1] = findarray[findarray.Length / 2];
+	return result;
+}
+int[] array = CreateArrayRndInt(5, 1, 9);
+PrintArray(array);
+int[] array2 = (ProdPairsNumbers(array));
+PrintArray(array2);
