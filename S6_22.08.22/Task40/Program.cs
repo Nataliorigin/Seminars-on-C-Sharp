84a4edb2a8654a66d66486583e8364a48060d648
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-08-24 17:17:18 +0500

    Seminar 6

---
+++ S6_22.08.22/Task40/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+bool Triangle(int num1, int num2, int num3)
+{
+	return num1 + num2 > num3 && num1 + num3 > num2 && num3 + num2 > num1;
+}
+
+bool result = Triangle (a, b, c);
+Console.WriteLine(result? "Тругольник с такими сторонами существует":"Тругольник с такими сторонами не существует");
---
+++ S6_22.08.22/Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите целое десятичное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int mult = 1;
+
+while (num > 0)
+{
+	result += num % 2 * mult;
+	num /= 2;
+	mult *= 10;
+}
+
+Console.Write(result);
+
+//Решение с рекурсией
+int number = 13;
+
+void DecToBin(int num)
+{
+	if (num == 0) return;
+	DecToBin(num / 2);
+	Console.Write(num % 2);
+}
+
+DecToBin(number);
